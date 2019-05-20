d9f7b88dfaba5402b2d182f88ae6e5fa914a2d5d
Jay Lewis <jr_lewis@outlook.com>
2019-05-20 12:50:30 +0100

    Implemented typeface ready for deplpoyment - missing about page

--- public/js/recording.js
+++ public/js/recording.js
@@ -55,245 +55,252 @@ function handlerFunction(stream) {
 
     submit.onclick = function(e) {
       e.preventDefault();
-      modal.style.display = "none";
+      console.log(trackInput.value);
+      if (trackInput.value === "") {
+        console.log("Enter a track name");
+        trackInput.setAttribute("id", "trackVal");
+      } else {
+        trackInput.setAttribute("id", "trackName");
+        modal.style.display = "none";
 
-      var trackName = trackInput.value;
+        var trackName = trackInput.value;
 
-      var trackContainer = document.createElement("article");
-      var trackLabel = document.createElement("p");
-      var audio = document.createElement("audio");
-      var waveDiv = document.createElement("div");
-      var deleteButton = document.createElement("button");
+        var trackContainer = document.createElement("article");
+        var trackLabel = document.createElement("p");
+        var audio = document.createElement("audio");
+        var waveDiv = document.createElement("div");
+        var deleteButton = document.createElement("button");
 
-      trackContainer.classList.add("tracks");
-      waveDiv.setAttribute("class", "waveform");
-      audio.setAttribute("class", "clip");
-      deleteButton.setAttribute("class", "delete-btn");
-      trackLabel.innerHTML = trackName;
-      trackLabel.setAttribute("class", "track-label");
+        trackContainer.classList.add("tracks");
+        waveDiv.setAttribute("class", "waveform");
+        audio.setAttribute("class", "clip");
+        deleteButton.setAttribute("class", "delete-btn");
+        trackLabel.innerHTML = trackName;
+        trackLabel.setAttribute("class", "track-label");
 
-      var btnDiv = document.createElement("div");
-      var playBtn = document.createElement("button");
-      var pauseBtn = document.createElement("button");
-      var play = document.createElement("img");
-      var pause = document.createElement("img");
+        var btnDiv = document.createElement("div");
+        var playBtn = document.createElement("button");
+        var pauseBtn = document.createElement("button");
+        var play = document.createElement("img");
+        var pause = document.createElement("img");
 
-      btnDiv.setAttribute("class", "btn-div");
-      playBtn.setAttribute("class", "func-btns");
-      pauseBtn.setAttribute("class", "func-btns");
+        btnDiv.setAttribute("class", "btn-div");
+        playBtn.setAttribute("class", "func-btns");
+        pauseBtn.setAttribute("class", "func-btns");
 
-      play.setAttribute("src", "../assets/play-icon.svg");
-      play.setAttribute("class", "play-icon");
-      pause.setAttribute("src", "../assets/pause-icon.svg");
-      pause.setAttribute("class", "pause-icon");
+        play.setAttribute("src", "../assets/play-icon.svg");
+        play.setAttribute("class", "play-icon");
+        pause.setAttribute("src", "../assets/pause-icon.svg");
+        pause.setAttribute("class", "pause-icon");
 
-      const playFnc = e => {
-        myaudio.play();
-        wavesurfer.play();
-      };
-      const pauseFnc = e => {
-        myaudio.pause();
-        wavesurfer.pause();
-      };
+        const playFnc = e => {
+          myaudio.play();
+          wavesurfer.play();
+        };
+        const pauseFnc = e => {
+          myaudio.pause();
+          wavesurfer.pause();
+        };
 
-      playBtn.onclick = playFnc;
-      pauseBtn.onclick = pauseFnc;
+        playBtn.onclick = playFnc;
+        pauseBtn.onclick = pauseFnc;
 
-      playBtn.appendChild(play);
-      pauseBtn.appendChild(pause);
+        playBtn.appendChild(play);
+        pauseBtn.appendChild(pause);
 
-      btnDiv.appendChild(playBtn);
-      btnDiv.appendChild(pauseBtn);
-      trackContainer.appendChild(trackLabel);
+        btnDiv.appendChild(playBtn);
+        btnDiv.appendChild(pauseBtn);
+        trackContainer.appendChild(trackLabel);
 
-      trackContainer.appendChild(btnDiv);
-      trackContainer.appendChild(audio);
-      trackContainer.appendChild(waveDiv);
-      trackContainer.appendChild(deleteButton);
-      soundClips.appendChild(trackContainer);
+        trackContainer.appendChild(btnDiv);
+        trackContainer.appendChild(audio);
+        trackContainer.appendChild(waveDiv);
+        trackContainer.appendChild(deleteButton);
+        soundClips.appendChild(trackContainer);
 
-      var blob = new Blob(audioChunks, {
-        type: "audio/ogg; codecs=opus"
-      });
-      audioChunks = [];
-      var audioURL = window.URL.createObjectURL(blob);
-      audio.src = audioURL;
-      var myaudio = new Audio(audioURL);
-
-      // sendData(blob);
-
-      // var checks = document.querySelectorAll(".tracks");
-
-      // const checkIndex = event => {
-      //   console.log(Array.from(checks).indexOf(event.target));
-      // };
-
-      // checks.forEach(
-      //   (func = check => {
-      //     check.addEventListener("click", checkIndex);
-      //   })
-      // );
-
-      var newLength = trackArray.unshift(trackContainer); // adds new recording to top of array
-
-      for (var x = 0; x < trackArray.length; x++) {
-        trackTable.appendChild(trackArray[x]);
-      }
-      deleteButton.onclick = e => {
-        var evtTgt = e.target;
-        evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-
-        var index = trackArray.indexOf(evtTgt.parentNode);
-
-        // console.log(index);
-
-        if (index === 0) {
-          trackArr.shift();
-          trackArray.shift();
-        } else {
-          trackArray.splice(index);
-          trackArr.splice(index);
-        }
-
-        // console.log(trackArray);
-        // console.log(trackArr);
-        // console.log(trackArray);
-      };
-
-      // console.log(blob);
-
-      var reader = new FileReader();
-      reader.readAsDataURL(blob);
-      reader.onloadend = function() {
-        base64data = reader.result;
-        // console.log(base64data);
-        // var stBlob = JSON.stringify(blob);
-        trackArr.push({
-          name: trackName,
-          audio: base64data
+        var blob = new Blob(audioChunks, {
+          type: "audio/ogg; codecs=opus"
         });
-      };
+        audioChunks = [];
+        var audioURL = window.URL.createObjectURL(blob);
+        audio.src = audioURL;
+        var myaudio = new Audio(audioURL);
 
-      // trackArr.push({ name: trackName, audio: blob });
+        // sendData(blob);
 
-      // Adds track data to the end of the array
-      // trackArr.push({ name: trackName, audio: base64data });
-      var saveBtn = document.getElementById("saveBtn");
+        // var checks = document.querySelectorAll(".tracks");
 
-      // console.log(project);
-      // console.log(blob);
+        // const checkIndex = event => {
+        //   console.log(Array.from(checks).indexOf(event.target));
+        // };
 
-      // freshGrid;
+        // checks.forEach(
+        //   (func = check => {
+        //     check.addEventListener("click", checkIndex);
+        //   })
+        // );
 
-      saveBtn.onclick = e => {
-        var lyricsElm = document.getElementById("lyrics");
-        var lyricsVal = lyricsElm.value;
-        var tabs = document.getElementsByClassName("freshGrid");
-        // console.log(tabs);
-        var tabVal = [];
-        if (tabs.length === 0) {
-          console.log("Tabs are not entered");
+        var newLength = trackArray.unshift(trackContainer); // adds new recording to top of array
 
-          // console.log(tabVal);
-          if (project === "") {
-            var val = document.getElementById("title-validation");
-            console.log("Error need a title");
-            val.style.display = "block";
+        for (var x = 0; x < trackArray.length; x++) {
+          trackTable.appendChild(trackArray[x]);
+        }
+        deleteButton.onclick = e => {
+          var evtTgt = e.target;
+          evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+
+          var index = trackArray.indexOf(evtTgt.parentNode);
+
+          // console.log(index);
+
+          if (index === 0) {
+            trackArr.shift();
+            trackArray.shift();
           } else {
-            axios({
-              method: "post",
-              url: "/projects",
-              data: {
-                title: project,
-                tracks: trackArr,
-                lyrics: lyricsVal,
-                tabs: ""
-              },
-              config: {
-                headers: {
-                  // "Content-Type": "application/x-www-form-urlencoded"
-                  "Content-Type": "multipart/form-data"
-                }
-              }
-            })
-              .then(function(response) {
-                console.log("Project Saved");
-                // Pop up box to say its saved
-                var popBox = document.getElementById("saveBox");
-                popBox.style.display = "block";
-                setTimeout(function() {
-                  popBox.style.display = "none";
-                }, 2000);
-              })
-              .catch(function(error) {
-                console.log(error);
-
-                // Put Validation
-              });
-            var popBox = document.getElementById("saveBox");
-            popBox.style.display = "block";
-            setTimeout(function() {
-              popBox.style.display = "none";
-            }, 2000);
+            trackArray.splice(index);
+            trackArr.splice(index);
           }
-        } else {
-          for (var a = 0; a < 6; a++) {
-            for (var b = 0; b < 12; b++) {
-              tabVal.push(
-                tabs[0].childNodes[a].childNodes[b].childNodes[0].innerHTML
-              );
+
+          // console.log(trackArray);
+          // console.log(trackArr);
+          // console.log(trackArray);
+        };
+
+        // console.log(blob);
+
+        var reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onloadend = function() {
+          base64data = reader.result;
+          // console.log(base64data);
+          // var stBlob = JSON.stringify(blob);
+          trackArr.push({
+            name: trackName,
+            audio: base64data
+          });
+        };
+
+        // trackArr.push({ name: trackName, audio: blob });
+
+        // Adds track data to the end of the array
+        // trackArr.push({ name: trackName, audio: base64data });
+        var saveBtn = document.getElementById("saveBtn");
+
+        // console.log(project);
+        // console.log(blob);
+
+        // freshGrid;
+
+        saveBtn.onclick = e => {
+          var lyricsElm = document.getElementById("lyrics");
+          var lyricsVal = lyricsElm.value;
+          var tabs = document.getElementsByClassName("freshGrid");
+          // console.log(tabs);
+          var tabVal = [];
+          if (tabs.length === 0) {
+            console.log("Tabs are not entered");
+
+            // console.log(tabVal);
+            if (project === "") {
+              var val = document.getElementById("title-validation");
+              console.log("Error need a title");
+              val.style.display = "block";
+            } else {
+              axios({
+                method: "post",
+                url: "/projects",
+                data: {
+                  title: project,
+                  tracks: trackArr,
+                  lyrics: lyricsVal,
+                  tabs: ""
+                },
+                config: {
+                  headers: {
+                    // "Content-Type": "application/x-www-form-urlencoded"
+                    "Content-Type": "multipart/form-data"
+                  }
+                }
+              })
+                .then(function(response) {
+                  console.log("Project Saved");
+                  // Pop up box to say its saved
+                  var popBox = document.getElementById("saveBox");
+                  popBox.style.display = "block";
+                  setTimeout(function() {
+                    popBox.style.display = "none";
+                  }, 2000);
+                })
+                .catch(function(error) {
+                  console.log(error);
+
+                  // Put Validation
+                });
+              var popBox = document.getElementById("saveBox");
+              popBox.style.display = "block";
+              setTimeout(function() {
+                popBox.style.display = "none";
+              }, 2000);
+            }
+          } else {
+            for (var a = 0; a < 6; a++) {
+              for (var b = 0; b < 12; b++) {
+                tabVal.push(
+                  tabs[0].childNodes[a].childNodes[b].childNodes[0].innerHTML
+                );
+              }
+            }
+            var val = document.getElementById("title-validation");
+            // console.log(tabVal);
+            if (project === "") {
+              console.log("Error need a title");
+              val.style.display = "block";
+            } else {
+              axios({
+                method: "post",
+                url: "/projects",
+                data: {
+                  title: project,
+                  tracks: trackArr,
+                  lyrics: lyricsVal,
+                  tabs: tabVal
+                },
+                config: {
+                  headers: {
+                    // "Content-Type": "application/x-www-form-urlencoded"
+                    "Content-Type": "multipart/form-data"
+                  }
+                }
+              });
+              var popBox = document.getElementById("saveBox");
+              popBox.style.display = "block";
+              setTimeout(function() {
+                popBox.style.display = "none";
+              }, 2000);
             }
           }
-          var val = document.getElementById("title-validation");
-          // console.log(tabVal);
-          if (project === "") {
-            console.log("Error need a title");
-            val.style.display = "block";
-          } else {
-            axios({
-              method: "post",
-              url: "/projects",
-              data: {
-                title: project,
-                tracks: trackArr,
-                lyrics: lyricsVal,
-                tabs: tabVal
-              },
-              config: {
-                headers: {
-                  // "Content-Type": "application/x-www-form-urlencoded"
-                  "Content-Type": "multipart/form-data"
-                }
-              }
-            });
-            var popBox = document.getElementById("saveBox");
-            popBox.style.display = "block";
-            setTimeout(function() {
-              popBox.style.display = "none";
-            }, 2000);
-          }
-        }
-        // console.log(trackArr);
-      };
+          // console.log(trackArr);
+        };
 
-      var wavesurfer = WaveSurfer.create({
-        container: ".waveform",
-        waveColor: "#FFFFFF",
-        progressColor: "white",
-        barGap: "4",
-        barWidth: "3",
-        barheight: "1",
-        height: "50",
-        scrollParent: false,
-        cursorWidth: "0",
-        interact: "true"
-      });
-      // console.log(trackArray);
-      wavesurfer.load(audioURL);
+        var wavesurfer = WaveSurfer.create({
+          container: ".waveform",
+          waveColor: "#FFFFFF",
+          progressColor: "white",
+          barGap: "4",
+          barWidth: "3",
+          barheight: "1",
+          height: "50",
+          scrollParent: false,
+          cursorWidth: "0",
+          interact: "true"
+        });
+        // console.log(trackArray);
+        wavesurfer.load(audioURL);
 
-      var titleDiv = document.getElementById("title-div");
+        var titleDiv = document.getElementById("title-div");
 
-      titleDiv.style.display = "inherit";
+        titleDiv.style.display = "inherit";
+      }
     };
   };
 
